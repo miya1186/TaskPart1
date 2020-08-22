484e12fec991603bd30d8a51ee09f97bf87bf4fd
miya1186 <66915986+miya1186@users.noreply.github.com>
2020-08-23 01:27:07 +0900

    first commit

---
+++ TaskPart1/TaskPart1/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TaskPart1
+//
+//  Created by miyazawaryohei on 2020/08/22.
+//  Copyright © 2020 miya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var numTextField1: UITextField!
+    @IBOutlet var numTextField2: UITextField!
+    @IBOutlet var numTextField3: UITextField!
+    @IBOutlet var numTextField4: UITextField!
+    @IBOutlet var numTextField5: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func add(_ sender: Any) {
+        let textArray :[String] = [
+            numTextField1.text!,
+            numTextField2.text!,
+            numTextField3.text!,
+            numTextField4.text!,
+            numTextField5.text!
+        ]
+        
+        let numArray = textArray.map({Int($0) ?? 0})
+        let CalcResult = numArray.reduce(0){$0 + $1}
+        resultLabel.text = String(CalcResult)
+    }
+}
+
